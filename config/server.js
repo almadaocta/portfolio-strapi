40ce68cc93fef1e6769a3027788c4edb670e7297
almadaocta <almadaocta@gmail.com>
2022-01-19 13:02:04 -0300

    new

--- config/server.js
+++ config/server.js
@@ -1,3 +1,9 @@
 module.exports = ({ env }) => ({
-  url: env('MY_HEROKU_URL'),
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', 'b12747e7fa4e22ff738283047c4e1359'),
+    },
+  },
 });
