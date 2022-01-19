c1bf76dab569ba7b149074c0ea70416bda772e16
almadaocta <almadaocta@gmail.com>
2022-01-19 12:19:01 -0300

    update db config

--- config/env/production/database.js
+++ config/env/production/database.js
@@ -5,11 +5,11 @@ module.exports = ({ env }) => ({
   connection: {
     client: 'postgres',
     connection: {
-      host: 'ec2-3-216-113-109.compute-1.amazonaws.com',
-      port: '5432',
-      database: 'd5mbmk608urdaq',
-      user: 'egpgyjjcdmcueg',
-      password: '259f2b2a8fe6db1d60a2f3191a81b621126038b4676b80215a98443381b2589c',
+      host: config.host,
+      port: config.port,
+      database: config.database,
+      user: config.user,
+      password: config.password,
       ssl: {
         rejectUnauthorized: false
       },
