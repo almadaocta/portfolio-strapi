063507b4d970afaafa9eb8d4004fe273d3def4f4
almadaocta <almadaocta@gmail.com>
2022-01-19 21:17:12 -0300

    postgres

--- config/database.js
+++ config/database.js
@@ -1,19 +1,19 @@
-const parse = require('pg-connection-string').parse;
-const config = parse(process.env.DATABASE_URL);
-module.exports = ({ env }) => ({
-  connection: {
-    client: 'postgres',
-    connection: {
-      host: config.host,
-      port: config.port,
-      database: config.database,
-      user: config.user,
-      password: config.password,
-      ssl: {
-        rejectUnauthorized: false
+    // strapi-api/config/database.js
+    module.exports = ({ env }) => ({
+      defaultConnection: "default",
+      connections: {
+        default: {
+          connector: "bookshelf",
+          settings: {
+            client: "postgres",
+            host: env("DATABASE_HOST", "localhost"),
+            port: env.int("DATABASE_PORT", 5432),
+            database: env("DATABASE_NAME", "bank"),
+            username: env("DATABASE_USERNAME", "postgres"),
+            password: env("DATABASE_PASSWORD", "0000"),
+            schema: env("DATABASE_SCHEMA", "public"),
+          },
+          options: {},
+        },
       },
-    },
-    debug: false,
-  },
-});
- 
+    });
